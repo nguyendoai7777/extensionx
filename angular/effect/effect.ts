161684336cde794a8d4596601117bda613f7fa03
doainn <49680790+nguyendoai7777@users.noreply.github.com>
2024-10-27 01:39:36 +0000

    Update effect.ts

--- angular/effect/effect.ts
+++ angular/effect/effect.ts
@@ -1,75 +1,4 @@
-import { computed, DestroyRef, effect, inject, isSignal, Signal } from '@angular/core';
-
-interface WatcherOption {
-  debounceTime?: number;
-}
-
-interface EffectOption extends WatcherOption {
-  destroyRef?: DestroyRef;
-}
-
-function _debounce(callback: () => void, delay: number) {
-  let timeout: ReturnType<typeof setTimeout> | null = null;
-  return () => {
-    if (timeout) clearTimeout(timeout);
-    timeout = setTimeout(callback, delay);
-  };
-}
-
-
-export type WatcherRef = (cleanupFn?: () => void) => void
-/**
- * something like useEffect of React
- *
- * @example
- * ```ts
- * @Component({})
- * class WatcherExample {
- *   cleanupWatcher!: WatcherRef
- *   count = signal(0)
- *   constructor() {
- *     this.cleanupWatcher = this.watcher(() => {
- *       // do something
- *     }, [this.count])
- *   }
- *   ngOnDestroy() {
- *     this.cleanupWatcher(() => {
- *       console.log(`Test Destroyed`);
- *     })
- *   }
- * }
- * ```
- * */
-
-export function watcher<T>(
-  callback: (values: T[]) => void,
-  deps: Signal<T>[],
-  option?: WatcherOption
-): WatcherRef {
-  if (!deps.every(isSignal)) {
-    throw new Error('All dependencies must be signals.');
-  }
-  const watchEffect = computed(() => deps.map(dep => dep()));
-  if (!option || !option?.debounceTime) {
-    option = {
-      debounceTime: 200
-    };
-  }
-  const debouncedCallback = _debounce(() => {
-    const values = watchEffect();
-    callback(values);
-  }, option.debounceTime!);
-
-  const cleanupEffect = effect(() => {
-    debouncedCallback();
-  });
-
-  return (cleanupFn) => {
-    cleanupFn && cleanupFn();
-    cleanupEffect.destroy();
-  };
-
-}
+import { DestroyRef, effect, inject, Signal } from '@angular/core';
 
 /**
  * something like useEffect of React
@@ -91,45 +20,21 @@ export function watcher<T>(
  * ```
  * */
 
-
 export function useEffect<T>(
-  callback: (values: T[]) => (() => void) | void,
+  callback: () => (() => void),
   deps: Signal<T>[],
-  option?: EffectOption
 ) {
-  if (!deps.every(isSignal)) {
-    throw new Error('All dependencies must be signals.');
-  }
-
-  const watchEffect = computed(() => deps.map(dep => dep()));
-  let cleanupFn: (() => void) | void;
-
-  if (!option) {
-    option = {};
-    if (!option.debounceTime) {
-      option.debounceTime = 200;
+  const destroyRef = inject(DestroyRef);
+  let cleanupFn: (() => void);
+  const stopEffect = effect(() => {
+    cleanupFn = callback();
+    for (const dep of deps) {
+      dep();
     }
-    if (!option.destroyRef) {
-      option.destroyRef = inject(DestroyRef);
-    }
-  }
-  const debouncedCallback = _debounce(() => {
-    if (cleanupFn) {
-      cleanupFn();
-    }
-
-    const values = watchEffect();
-    cleanupFn = callback(values);
-  }, option.debounceTime!);
-
-  const cleanupEffect = effect(() => {
-    debouncedCallback();
   });
 
-  option.destroyRef!.onDestroy(() => {
-    if (cleanupFn) {
-      cleanupFn();
-    }
-    cleanupEffect.destroy();
+  destroyRef.onDestroy(() => {
+    stopEffect.destroy();
+    cleanupFn();
   });
 }

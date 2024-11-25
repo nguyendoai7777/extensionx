4978169cc22d1a7e3de70a673013664adf135ee8
doainn <49680790+nguyendoai7777@users.noreply.github.com>
2024-11-25 10:51:06 +0700

    Update typed-form.type.ts

--- angular/utils/typed-form.type.ts
+++ angular/utils/typed-form.type.ts
@@ -1,15 +1,45 @@
 import { FormArray, FormControl, FormGroup } from '@angular/forms';
 
-export type TypedFormGroup<TFormData extends object> = FormGroup<{
-  [TFormKey in keyof TFormData]: TFormData[TFormKey] extends Date
-    ? FormControl<Date>
-    : TFormData[TFormKey] extends (infer TItem)[]
-    ? TItem extends Date
-      ? FormControl<Date[]>
-      : TItem extends object
-      ? FormArray<TypedFormGroup<TItem>>
-      : FormControl<TItem[]>
-    : TFormData[TFormKey] extends object
-    ? TypedFormGroup<TFormData[TFormKey]>
-    : FormControl<TFormData[TFormKey]>;
+/*
+export type NgTypedForm<TFormData extends object> = FormGroup<{
+	[TFormKey in keyof TFormData]: TFormData[TFormKey] extends Date
+		? FormControl<Date>
+		: TFormData[TFormKey] extends (infer TItem)[]
+			? TItem extends Date
+				? FormControl<Date[]>
+				: TItem extends object
+					? FormArray<NgTypedForm<TItem>>
+					: FormControl<TItem[]>
+			: TFormData[TFormKey] extends object
+				? NgTypedForm<TFormData[TFormKey]>
+				: FormControl<TFormData[TFormKey]>;
 }>;
+*/
+
+type TypedFormControl<T> = T extends Date
+  ? FormControl<Date>
+  : FormControl<T>;
+
+// Helper type để xử lý form group
+type TypedFormGroup<T> = FormGroup<{
+  [K in keyof T]: TypedControl<T[K]>;
+}>;
+
+// Helper type để xử lý form array
+type TypedFormArray<T> = FormArray<
+  T extends (infer U)[]
+    ? U extends object
+      ? TypedFormGroup<U>
+      : TypedFormControl<U>
+    : never
+>;
+
+// Main type để xử lý tất cả các trường hợp
+type TypedControl<T> = T extends (infer _U)[]
+  ? TypedFormArray<T>
+  : T extends object
+    ? TypedFormGroup<T>
+    : TypedFormControl<T>;
+
+// Type chính để sử dụng
+export type NgTypedForm<TFormData extends object> = TypedFormGroup<TFormData>;

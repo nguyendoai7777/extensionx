902bec1b97c6d25c9b6c266e3c912a3d64229fe0
doainn <49680790+nguyendoai7777@users.noreply.github.com>
2025-08-28 10:23:45 +0700

    Update local-storage.ts

--- utils/local-storage.ts
+++ utils/local-storage.ts
@@ -34,3 +34,37 @@ export class LS {
 		localStorage.clear();
 	}
 }
+
+// as const
+export const StorageLocal = {
+  setItem: (key: string, data: unknown) => {
+    if (typeof data === 'object') {
+      localStorage.setItem(key, JSON.stringify(data));
+    } else {
+      localStorage.setItem(key, data as string);
+    }
+  },
+  getItem: <TData = string>(key: string): (TData extends object ? TData : string) | null => {
+    const item = localStorage.getItem(key);
+    if (!item) {
+      return null;
+    }
+    try {
+      const parsed = JSON.parse(item);
+      if (typeof parsed === 'object') {
+        return parsed;
+      }
+      return item as TData extends object ? TData : string;
+    } catch (e) {
+      return item as TData extends object ? TData : string;
+    }
+  },
+
+  removeItem: (key: string) => {
+    localStorage.removeItem(key);
+  },
+  clear: () => {
+    localStorage.clear();
+  },
+};
+

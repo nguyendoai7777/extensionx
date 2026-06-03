c606f4053e7fb9f2860ce8dd6440f92306bb0e49
doainn <49680790+nguyendoai7777@users.noreply.github.com>
2026-06-03 10:51:56 +0700

    Delete angular/use-signal directory

--- angular/use-signal/use-signal.d.ts
+++
@@ -1,6 +0,0 @@
-import { WritableSignal } from '@angular/core';
-
-declare function useSignal<S>(initialValue: S | (() => S)): WritableSignal<S>;
-declare function useSignal<S = undefined>(): WritableSignal<S | undefined>;
-
-export { useSignal };

f4fc43e7dc84a8e795fa2568e8f3ed511e8669a8
doainn <49680790+nguyendoai7777@users.noreply.github.com>
2026-06-05 16:22:40 +0700

    Update common.ts

--- types/common.ts
+++ types/common.ts
@@ -3,3 +3,21 @@ export type Prettify<T> = {
 } & {}
 
 export type CastString<T> = T | (string & {})
+
+type XRange<
+  TStart extends number,
+  TEnd extends number,
+  TAcc extends number[] = [],
+  TResult extends number = never
+> = TAcc['length'] extends TEnd
+  ? TResult | TEnd
+  : XRange<
+    TStart,
+    TEnd,
+    [...TAcc, TAcc['length']],
+    TAcc['length'] extends TStart
+      ? TStart
+      : TResult extends never
+        ? never
+        : TResult | TAcc['length']
+  >
